c179a9d614127c4c388bb6f44c4b5bf4d54b6f63
David Sudia <dsudia@gmail.com>
2016-02-24 15:20:41 -0700

    create seed file, write schema code for creating tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,2 +1,42 @@
-DROP DATABASE IF EXISTS hogwarts_[FIRSTNAME]_[LASTNAME];
-CREATE DATABASE hogwarts_[FIRSTNAME]_[LASTNAME];
+DROP DATABASE IF EXISTS hogwarts_david_sudia;
+CREATE DATABASE hogwarts_david_sudia;
+
+create table houses (
+  id serial primary key,
+  name varchar(50) not null
+)
+
+create table parents (
+  id serial primary key,
+  name varchar (50)
+);
+
+create table students (
+  id serial primary key,
+  name varchar(50) not null,
+  year integer not null,
+  house_id integer references houses(id)
+);
+
+create table teachers (
+  id serial primary key,
+  house_id integer references houses(id)
+);
+
+create table classes (
+  id serial primary key,
+  subject varchar(50),
+  teacher_id integer references teachers(id)
+);
+
+create table par_child_rels (
+  id serial primary key,
+  student_id integer references students(id),
+  parent_id integer references students(id)
+)
+
+create table rosters (
+  id serial primary key,
+  student_id integer references students(id),
+  class_id integer references classes(id)
+)
